8c76a21bb444cec159d7720c99ee98d6244aba46
zainab baruud <zainabbaruud@ma373.home>
2021-09-29 09:26:23 +0100

    adding a test for credit in statement

---
+++ lib/statement.rb
@@ -0,0 +1,8 @@
+class Statement
+  attr_reader :date
+
+  def initialize(date, credit, debit, balance)
+  @date = date
+  end
+  
+end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,9 +1,15 @@
+require 'statement'
+
 describe Statement do
   subject(:statement) { described_class.new('10/01/2023', 1000, 0, 1000) }
 
   describe '#initialize' do
     it 'should have a date' do
       expect(statement.date).to eq('10/01/2023')
+    end
+
+    it 'should have a credit amount' do
+      expect(statement.credit).to eq(1000)
     end
   end
 end
