cd46c06f3130029e2cfea22e89143656375cc53d
zainab baruud <zainabbaruud@MA373.local>
2021-09-29 15:34:54 +0100

    I'm starting again from the bank class, I had too many classes and confused myself

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,11 +1,4 @@
 class Statement
-  attr_reader :date, :credit, :debit, :balance
 
-  def initialize(date, credit, debit, balance)
-  @date = date
-  @credit = credit
-  @debit = debit
-  @balance = balance
-  end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,23 +1,5 @@
 require 'statement'
 
 describe Statement do
-  subject(:statement) { described_class.new('10/01/2023', 1000, 0, 1000) }
-
-  describe '#initialize' do
-    it 'should have a date' do
-      expect(statement.date).to eq('10/01/2023')
-    end
-
-    it 'should have a credit amount' do
-      expect(statement.credit).to eq(1000)
-    end
-
-    it 'should have a debit amount' do
-      expect(statement.debit).to eq(0)
-    end
-
-    it 'should have a balance' do
-      expect(statement.balance).to eq(1000)
-    end
-  end
+  
 end
