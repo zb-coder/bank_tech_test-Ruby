52f8a4d7c5201dcae22080314cb71c23e41fe5c0
zainab baruud <zainabbaruud@MA373.local>
2021-09-29 21:01:42 +0100

    Adding test and hardcoaded header to the statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,4 +1,7 @@
 class Statement
 
+  def statement_header
+    puts "date || credit || debit || balance"
+  end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,5 +1,8 @@
 require 'statement'
 
 describe Statement do
-  
+  let(:statement) { described_class.new }
+  it 'should have a header like the one in the read me' do
+    expect {statement.statement_header }.to output("date || credit || debit || balance\n").to_stdout
+  end
 end
